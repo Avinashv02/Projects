09081ee47e73f8ab0b6a0cf96be85b3e812ec2ca
Avinashv02 <165052178+Avinashv02@users.noreply.github.com>
2024-11-19 16:55:06 +0530

    Update app.py

--- Chatbot/src/app.py
+++ Chatbot/src/app.py
@@ -41,7 +41,7 @@ theme = st.sidebar.selectbox("Choose Theme", ["Light", "Dark", "Material"])
 light_theme_css = """
     <style>
     body { background-color: #FAFAFA; color: linear-gradient(to left, #4285f4, #d96570); }
-    .main-header { color: linear-gradient(to left, #4285f4, #d96570); text-align: center; font-family: 'Roboto', sans-serif; }
+    .main-header { color: #5BBCFF; text-align: center; font-family: 'Roboto', sans-serif; }
     .stButton button { background:  linear-gradient(to left, #4285f4, #d96570); color: white; border-radius: 8px; }
     .stButton button:hover { background:  linear-gradient(to right, #4285f4, #d96570); color: white; }
     .response-text { background-color: #FFFFFF; border: 1px solid #E0E0E0; color: #424242; padding: 20px; border-radius: 8px; }
