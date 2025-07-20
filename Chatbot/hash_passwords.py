45dc6972e8a784156754510d7cc28b40f83dfafe
Avinashv02 <165052178+Avinashv02@users.noreply.github.com>
2025-07-20 22:02:56 +0530

    Delete Chatbot directory

--- Chatbot/hash_passwords.py
+++
@@ -1,9 +0,0 @@
-import bcrypt
-
-def hash_password(plain_password):
-    return bcrypt.hashpw(plain_password.encode(), bcrypt.gensalt()).decode()
-
-passwords = ["password123"]  # Replace with your actual plaintext passwords
-hashed_passwords = [hash_password(password) for password in passwords]
-
-print("Hashed Passwords:", hashed_passwords)
